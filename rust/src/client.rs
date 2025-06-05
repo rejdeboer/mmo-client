ece94d42039f07e29212b05c45a3cf616306a86c
rejdeboer <rick.deboer@live.nl>
2025-06-05 17:31:23 +0200

    feat: boilerplate to connect to server

--- rust/src/client.rs
+++ rust/src/client.rs
@@ -15,3 +15,20 @@ impl INode3D for NetworkManager {
         self.client.update(Duration::from_secs_f64(dt));
     }
 }
+
+#[godot_api]
+impl NetworkManager {
+    // TODO: Add character to body
+    #[signal]
+    fn enter_game_success();
+
+    #[func]
+    pub fn connect(&mut self, host: String, port: u16) {
+        self.client.connect(host, port);
+    }
+
+    #[func]
+    pub fn send_enter_game_request(&mut self, character_id: i32, token: String) {
+        self.client.send_enter_game_request(character_id, token);
+    }
+}
