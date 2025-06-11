09c9a0c0050a2612d22b018afdd639a5fd3afeab
rejdeboer <rick.deboer@live.nl>
2025-06-11 10:43:10 +0200

    chore: remove unused node

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,17 +4,5 @@ use godot::prelude::*;
 
 struct MyExtension;
 
-#[derive(GodotClass)]
-#[class(init)]
-struct TestStruct {}
-
-#[godot_api]
-impl TestStruct {
-    #[func]
-    fn print(&mut self) {
-        godot_print!("Print from extension!");
-    }
-}
-
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
