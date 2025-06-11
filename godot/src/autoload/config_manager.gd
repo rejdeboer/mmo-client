86d008e20c6b0eb057a963212e94b17c482337ca
rejdeboer <rick.deboer@live.nl>
2025-06-11 19:04:24 +0200

    feat: config manager autoload

--- godot/src/autoload/config_manager.gd
+++ godot/src/autoload/config_manager.gd
@@ -2,6 +2,11 @@ extends Node
 
 var config = ConfigFile.new()
 
-# TODO: Do we need this?
 func _ready() -> void:
-	var _err = config.load("res://config.cfg")
+	var err = config.load("res://config.cfg")
+	if err != OK:
+		# TODO: Load default config
+		print("error loading config file: " + err)
+
+func get_value(section: String, key: String):
+	return config.get_value(section, key)
