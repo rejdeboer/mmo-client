b150aa92a9473587e9d514e1b6f79e9f9a35cd83
rejdeboer <rick.deboer@live.nl>
2025-06-19 19:22:38 +0200

    feat: spring arm camera

--- godot/game/player.gd
+++ godot/game/player.gd
@@ -5,6 +5,12 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
+@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
+@export var tilt_limit = deg_to_rad(75)
+
+@onready var _camera := %Camera3D as Camera3D
+@onready var _camera_pivot := %CameraPivot as Node3D
+
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
@@ -32,3 +38,10 @@ func _physics_process(delta):
 
 		velocity = target_velocity
 		move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		_camera_pivot.rotation.x -= event.relative.y * mouse_sensitivity
+		# Prevent the camera from rotating too far up or down.
+		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
+		_camera_pivot.rotation.y += -event.relative.x * mouse_sensitivity
