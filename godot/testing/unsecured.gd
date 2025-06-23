78db105d293b43420ed84b24bd54430e10037346
rejdeboer <rick.deboer@live.nl>
2025-06-23 14:11:35 +0200

    feat: simplify unsecured scene

--- godot/testing/unsecured.gd
+++ godot/testing/unsecured.gd
@@ -5,25 +5,12 @@ extends Control
 @onready var ip_address_edit: LineEdit = $IPAddressEdit
 @onready var port_edit: LineEdit = $PortEdit
 @onready var connect_button: Button = $ConnectButton
-@onready var message_edit: LineEdit = $MessageEdit
-@onready var send_button: Button = $SendButton
 @onready var status_label: Label = $StatusLabel
 
 func _ready() -> void:
 	connect_button.pressed.connect(_on_connect_button_pressed)
-	send_button.pressed.connect(_on_send_button_pressed)
-	send_button.disabled = true # Start with send button disabled
 
 func _on_connect_button_pressed() -> void:
-	# if is_connected:
-	# 	# Disconnect logic (optional for this simple test, or add a disconnect button)
-	# 	is_connected = false
-	# 	status_label.text = "Status: Disconnected"
-	# 	connect_button.text = "Connect"
-	# 	send_button.disabled = true
-	# 	printerr("Disconnected.")
-	# 	return
-
 	var ip: String = ConfigManager.game_server.host
 	var port: int = ConfigManager.game_server.port
 
@@ -35,59 +22,9 @@ func _on_connect_button_pressed() -> void:
 	status_label.text = "Status: Connecting..."
 	NetworkManager.connect_unsecure(ip, port, 0)
 
-	# if err != OK:
-	# 	status_label.text = "Status: Connection failed. Error: " + str(err)
-	# 	printerr("Connection failed. Error: " + str(err))
-	# 	is_connected = false
-	# 	return
-
-	# Connection attempt started. We need to poll status in _process
-	# For simplicity here, we'll assume it connects quickly or fails.
-	# A more robust solution would poll client.get_status() in _process
-	# until STATUS_CONNECTED or STATUS_ERROR.
-
 
 func _on_send_button_pressed() -> void:
 	print("send button pressed")
-	# if not is_connected:
-	# 	status_label.text = "Status: Not connected. Cannot send."
-	# 	printerr("Not connected. Cannot send.")
-	# 	return
-	#
-	# var message_to_send: String = message_edit.text
-	# if message_to_send.is_empty():
-	# 	status_label.text = "Status: Message is empty."
-	# 	return
-	#
-	# var data_to_send: PackedByteArray = message_to_send.to_utf8_buffer()
-	# var err: Error = client.put_data(data_to_send)
-	#
-	# if err != OK:
-	# 	status_label.text = "Status: Error sending data: " + str(err)
-	# 	printerr("Error sending data: " + str(err))
-	# else:
-	# 	status_label.text = "Status: Sent: '" + message_to_send + "'"
-	# 	print("Sent: '", message_to_send, "'")
-
-func _process(_delta: float) -> void:
-	if not is_connected:
-		return
-
-	# # Check connection status
-	# if manager.is
-	#
-	# # Try to receive data
-	# var available_bytes: int = client.get_available_bytes()
-	# if available_bytes > 0:
-	# 	var received_data: PackedByteArray = client.get_data(available_bytes)[1] # get_data returns [error_code, data_array]
-	# 	if received_data.size() > 0:
-	# 		var received_string: String = received_data.get_string_from_utf8()
-	# 		status_label.text = "Status: Received: '" + received_string + "'"
-	# 		print("Received: '", received_string, "'")
-	# 	else:
-	# 		# This might happen if get_data failed internally after available_bytes > 0
-	# 		# or if the server sent an empty payload that still registered bytes.
-	# 		print("Received 0 bytes despite available_bytes > 0, or error in get_data.")
 
 
 func _on_connection_success() -> void:
