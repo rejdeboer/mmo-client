69f3e7afebc7042d6bfebcf794ccea88525081a7
rejdeboer <rick.deboer@live.nl>
2025-07-02 13:32:23 +0200

    feat: do transform conversions correctly

--- rust/src/client.rs
+++ rust/src/client.rs
@@ -34,12 +34,13 @@ impl From<mmo_client::Character> for Character {
 }
 
 pub fn convert_transform(transform: mmo_client::Transform) -> Transform3D {
+    let basis = Basis::from_axis_angle(Vector3::new(0., 1., 0.), transform.yaw);
     let pos = Vector3::new(
         transform.position.x,
         transform.position.y,
         transform.position.z,
     );
-    Transform3D::new(Basis::default(), pos).rotated(Vector3::new(0., 1., 0.), transform.yaw)
+    Transform3D::new(basis, pos)
 }
 
 #[derive(GodotClass)]
