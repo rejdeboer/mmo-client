6f8ff05a4bfe1d22ea4320ccbf5001f781187238
rejdeboer <rick.deboer@live.nl>
2025-07-08 18:27:26 +0200

    feat: dev tooling for starting multiple unsecured connections

---
+++ godot/tools/unsecured.gd
@@ -0,0 +1,20 @@
+# NOTE: Allows to start the game without a connect token
+# Some arguments are available via command line
+# 1: Account email
+# 2: Account password
+# 3: Character name
+# These can be set up in "Debug" -> "Customize run instances..."
+# WARNING: Only use for testing
+extends Control
+
+func _ready() -> void:
+	var ip: String = ConfigManager.game_server.host
+	var port: int = ConfigManager.game_server.port
+
+	if ip.is_empty() or port <= 0 or port > 65535:
+		printerr("Invalid IP or Port.")
+		return
+
+	var character_id = OS.get_cmdline_args().get(3)
+
+	NetworkManager.connect_unsecure(ip, port, int(character_id))
