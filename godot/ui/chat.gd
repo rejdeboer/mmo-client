86f9433c302f8d6dd9dc218c285b89c8b08b2c94
rejdeboer <rick.deboer@live.nl>
2025-07-16 13:43:26 +0200

    feat: start creating chat widget

---
+++ godot/ui/chat.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@onready var chat_display = $VBoxContainer/Display
+@onready var message_input = $VBoxContainer/ChatInput
+
+func _ready():
+	message_input.text_submitted.connect(_on_text_submitted)
+	
+func _process(delta):
+	if Input.is_action_pressed("enter"):
+		if message_input.has_focus():
+			send_message()
+		else:
+			message_input.grab_focus()
+
+func _on_text_submitted(text):
+	pass
+	#send_message()
+
+func send_message():
+	var text = message_input.text
+	if text.strip_edges().is_empty():
+		return
+
+	add_message("You", text)
+
+	message_input.text = ""
+	message_input.grab_focus()
+
+func add_message(username, message):
+	chat_display.append_text("[b]" + username + ":[/b] " + message + "\n")
