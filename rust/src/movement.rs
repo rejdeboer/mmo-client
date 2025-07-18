5d649338839ef93bd64a863835c089bef7cf972b
rejdeboer <rick.deboer@live.nl>
2025-07-18 19:12:42 +0200

    chore: indentation

--- rust/src/movement.rs
+++ rust/src/movement.rs
@@ -17,16 +17,12 @@ pub fn read_movement_bytes(bytes: PackedByteArray) -> Option<MoveAction> {
 
 fn parse_action(cursor: &mut Cursor<&[u8]>) -> Result<MoveAction, std::io::Error> {
     let mut buf = [0u8; 4];
-
     cursor.read_exact(&mut buf)?;
     let pos_x = f32::from_le_bytes(buf);
-
     cursor.read_exact(&mut buf)?;
     let pos_y = f32::from_le_bytes(buf);
-
     cursor.read_exact(&mut buf)?;
     let pos_z = f32::from_le_bytes(buf);
-
     cursor.read_exact(&mut buf)?;
     let yaw = f32::from_le_bytes(buf);
 
