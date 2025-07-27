41b9e622dac2b60fb0adef9c6056f1605b24c73f
rejdeboer <rick.deboer@live.nl>
2025-07-27 21:07:54 +0200

    feat: register async runtime singleton

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,6 +4,7 @@ mod movement;
 
 use godot::classes::Engine;
 use godot::prelude::*;
+use godot_tokio::AsyncRuntime;
 
 use crate::client::NetworkManager;
 
@@ -14,19 +15,22 @@ unsafe impl ExtensionLibrary for NetcodeExtension {
     fn on_level_init(level: InitLevel) {
         if level == InitLevel::Scene {
             Engine::singleton().register_singleton("NetworkManager", &NetworkManager::new_alloc());
+            Engine::singleton()
+                .register_singleton(AsyncRuntime::SINGLETON, &AsyncRuntime::new_alloc());
         }
     }
 
     fn on_level_deinit(level: InitLevel) {
         if level == InitLevel::Scene {
             let mut engine = Engine::singleton();
-            let singleton_name = "NetworkManager";
-
-            if let Some(my_singleton) = engine.get_singleton(singleton_name) {
-                engine.unregister_singleton(singleton_name);
-                my_singleton.free();
-            } else {
-                godot_error!("Failed to get singleton");
+            let singletons = vec!["NetworkManager", AsyncRuntime::SINGLETON];
+            for singleton_name in singletons.into_iter() {
+                if let Some(singleton) = engine.get_singleton(singleton_name) {
+                    engine.unregister_singleton(singleton_name);
+                    singleton.free();
+                } else {
+                    godot_error!("Failed to get singleton: {:?}", singleton_name);
+                }
             }
         }
     }
