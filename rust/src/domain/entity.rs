716a7881c3f20c4e9dddae7b0a3cbd42822f53e6
rejdeboer <rick.deboer@live.nl>
2025-08-16 14:44:03 +0200

    feat: add entity attributes to godot entity

--- rust/src/domain/entity.rs
+++ rust/src/domain/entity.rs
@@ -1,5 +1,6 @@
 use crate::domain::convert_transform;
 use godot::prelude::*;
+use mmo_client::EntityAttributes;
 
 #[derive(GodotClass, Debug, Clone)]
 #[class(base=RefCounted, init)]
@@ -14,16 +15,43 @@ pub struct Entity {
     pub level: i32,
     #[var]
     pub transform: Transform3D,
+    #[export]
+    pub attributes: Variant,
 }
+
+#[derive(GodotClass, Debug)]
+#[class(base=RefCounted, init)]
+pub struct PlayerAttributes {
+    #[export]
+    pub character_id: i32,
+    #[export]
+    pub guild_name: GString,
+}
+
+#[derive(GodotClass, Debug)]
+#[class(base=RefCounted, init)]
+pub struct NpcAttributes {}
 
 impl From<mmo_client::Entity> for Entity {
     fn from(entity: mmo_client::Entity) -> Self {
+        let attributes = match entity.attributes {
+            EntityAttributes::Player {
+                character_id,
+                guild_name,
+            } => Variant::from(Gd::from_object(PlayerAttributes {
+                character_id,
+                guild_name: guild_name.map_or(GString::new(), GString::from),
+            })),
+            EntityAttributes::Npc => Variant::from(Gd::from_object(NpcAttributes {})),
+        };
+
         Self {
             entity_id: entity.id as i64,
             name: GString::from(entity.name),
             hp: entity.vitals.hp,
             level: entity.level,
             transform: convert_transform(entity.transform),
+            attributes,
         }
     }
 }
