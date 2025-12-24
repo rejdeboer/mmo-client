fae8fa636cc4bc8765c3b21afe994420db1e9dd1
rejdeboer <rick.deboer@live.nl>
2025-12-24 14:28:45 +0100

    feat: play entity animations

--- godot/game/entity.gd
+++ godot/game/entity.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 var entity_data: Entity
 var target_transform = Transform3D()
 var entity_template: EntityTemplate
+var animation_player: AnimationPlayer
 
 
 func setup(entity: Entity):
@@ -21,15 +22,18 @@ func setup(entity: Entity):
 
 func _ready():
 	target_transform = self.transform
-	pivot.add_child(entity_template.scene.instantiate())
+	var character = entity_template.scene.instantiate()
+	pivot.add_child(character)
+	character.name = "Character"
+	animation_player = character.get_node("AnimationPlayer")
 
 
 func _physics_process(delta):
 	transform = transform.interpolate_with(target_transform, 0.2)
-	# if transform.origin.distance_to(target_transform.origin) >= 0.05:
-	# 	%Character/AnimationPlayer.current_animation = "run"
-	# else:
-	# 	%Character/AnimationPlayer.current_animation = "idle"
+	if transform.origin.distance_to(target_transform.origin) >= 0.05:
+		animation_player.current_animation = "Running_A"
+	else:
+		animation_player.current_animation = "Idle_A"
 
 
 func _input_event(
